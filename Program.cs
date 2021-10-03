70de440fb65b02d4bc25042105b614ad13c2544a
umamahale <umagasmos@gmail.com>
2021-10-02 21:22:25 -0400

    first message

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static FindMaximumNumberProject.R2GenricMaximum;
+
+namespace FindMaximumNumberProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Find max Number");
+            //int output = R1MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            //Console.WriteLine(output);
+            //double doubleoutput = R1MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
+            //Console.WriteLine(doubleoutput);
+            //string strigoutput = R1MaximumNumberCheck.MaximumStringNumber("22", "33", "44");
+            //Console.WriteLine(strigoutput);
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
+            double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "111", "222", "333", "55", "999" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            //GenericMaximum<Employee> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
+            Console.ReadKey();
+
+        }
+    }
+}
